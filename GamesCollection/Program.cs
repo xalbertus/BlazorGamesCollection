61e269404cf47c77ffcad2cfc03ee15b8adf1b1f
xalbertus <x.alber.latt@gmail.com>
2025-04-25 15:11:37 +0200

    Game model scaffolded

--- GamesCollection/Program.cs
+++ GamesCollection/Program.cs
@@ -1,4 +1,4 @@
-using GamesCollection.Components;
+﻿using GamesCollection.Components;
 using GamesCollection.Components.Account;
 using GamesCollection.Data;
 using Microsoft.AspNetCore.Components.Authorization;
@@ -26,6 +26,8 @@ builder.Services.AddAuthentication(options =>
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
@@ -47,6 +49,7 @@ else
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseMigrationsEndPoint();
 }
 
 app.UseHttpsRedirection();
